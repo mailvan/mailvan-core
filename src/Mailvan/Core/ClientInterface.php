51f0e76e79b685189b22caa07f7095409a580245
enl <8a4G7EPe>
2014-01-24 17:37:48 +0700

    Added methods for Client's real job)

--- src/Mailvan/Core/ClientInterface.php
+++ src/Mailvan/Core/ClientInterface.php
@@ -3,6 +3,8 @@
 namespace Mailvan\Core;
 
 use Guzzle\Service\ClientInterface as BaseClientInterface;
+use Mailvan\Core\Model\SubscriptionListInterface;
+use Mailvan\Core\Model\UserInterface;
 
 /**
  * Project-specific guzzle client extension
@@ -12,6 +14,41 @@ use Guzzle\Service\ClientInterface as BaseClientInterface;
  */
 interface ClientInterface extends BaseClientInterface
 {
+    /**
+     * Subscribes given user to given SubscriptionList. Returns true if operation is successful
+     *
+     * @param UserInterface $user
+     * @param SubscriptionListInterface $list
+     * @return boolean
+     */
+    public function subscribe(UserInterface $user, SubscriptionListInterface $list);
+
+    /**
+     * Unsubscribes given user from given SubscriptionList.
+     *
+     * @param UserInterface $user
+     * @param SubscriptionListInterface $list
+     * @return boolean
+     */
+    public function unsubscribe(UserInterface $user, SubscriptionListInterface $list);
+
+    /**
+     * Moves user from one list to another. In some implementation can create several http queries.
+     *
+     * @param UserInterface $user
+     * @param SubscriptionListInterface $from
+     * @param SubscriptionListInterface $to
+     * @return boolean
+     */
+    public function move(UserInterface $user, SubscriptionListInterface $from, SubscriptionListInterface $to);
+
+    /**
+     * Returns list of subscription lists created by owner.
+     *
+     * @return array
+     */
+    public function getLists();
+
     /**
      * Creates instance of SubscriptionListInterface from given data
      * @param int $id
