9354d2b5991e338cfedb51470dcabd094fb5c1af
enl <8a4G7EPe>
2014-02-24 15:15:28 +0700

    Added method Client::doExecuteCommand and supporting abstract methods.

--- src/Mailvan/Core/Client.php
+++ src/Mailvan/Core/Client.php
@@ -32,4 +32,49 @@ abstract class Client extends BaseClient implements ClientInterface
     {
         return User::fromArray($user_info);
     }
+
+    /**
+     * @param $command
+     * @param $params
+     * @param callable $responseParser
+     *
+     * @throws MailvanException
+     * @return mixed
+     */
+    protected function doExecuteCommand($command, $params, \Closure $responseParser)
+    {
+        $params = $this->mergeApiKey($params);
+
+        $response = $this->getCommand($command, $params)->getResult();
+        if ($this->hasError($response)) {
+            throw $this->raiseError($response);
+        }
+
+        return $responseParser($response);
+    }
+
+    /**
+     * Merge API key into params array. Some implementations require to do this.
+     *
+     * @param $params
+     * @return mixed
+     */
+    abstract protected function mergeApiKey($params);
+
+    /**
+     * Check if server returned response containing error message.
+     * This method must return true if servers does return error.
+     *
+     * @param $response
+     * @return mixed
+     */
+    abstract protected function hasError($response);
+
+    /**
+     * Raises Exception from response data
+     *
+     * @param $response
+     * @return MailvanException
+     */
+    abstract protected function raiseError($response);
 }
---
+++ src/Mailvan/Core/MailvanException.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Mailvan\Core;
+
+
+class MailvanException extends \RuntimeException
+{
+
+} 
